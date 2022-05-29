f36a78b12856739183336739b87307ce32ee00bb
iliana etaoin <iliana@buttslol.net>
2022-05-28 18:01:20 -0700

    what are you, a cop

--- kijetesantakaluotokieni.rb
+++ kijetesantakaluotokieni.rb
@@ -1,5 +1,7 @@
 class Kijetesantakaluotokieni < Formula
+  # rubocop:disable Style/DisableCopsWithinSourceCodeDirective, FormulaAudit/Desc
   desc "kijetesantakalu toki pi ken ante mute"
+  # rubocop:enable Style/DisableCopsWithinSourceCodeDirective, FormulaAudit/Desc
   homepage "https://git.2ki.xyz/spiders/kijetesantakaluotokieni"
   url "https://static.crates.io/crates/kijetesantakaluotokieni/kijetesantakaluotokieni-1.0.1.crate"
   sha256 "adf46be7b001c78b3e8ece243dc84f77d86261ce4d00460bc1cbc958adc39063"
